b0027ede01df77a76fa9ca9baad5b8335e355ce6
Douglas <dug.armadale@gmail.com>
2017-02-11 20:21:02 +0000

    Info about unit testing GAE database.

--- GAE/python_code/test.py
+++ GAE/python_code/test.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# https://cloud.google.com/appengine/docs/python/tools/localunittesting
+
+#http://einaregilsson.com/unit-testing-model-classes-in-google-app-engine/
+
+import unittest
+
 import os
 import sys
 
@@ -60,8 +66,26 @@ process_and_render_html.init_template_environment(path_to_templates)
 
 process_and_render_html.get_index_page_html("", "")
 
-sold_ticket_list.append("0", "A", "Not used")
 
+class DatastoreTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        # Clear ndb's in-context cache between tests.
+        # This prevents data from leaking between tests.
+        # Alternatively, you could disable caching by
+        # using ndb.get_context().set_cache_policy(False)
+        ndb.get_context().clear_cache()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+#sold_ticket_list.append("0", "A", "Not used")
 #process_and_render_html.get_buy_tickets_dummy_charge_html({},{}, sold_ticket_list)
 
     ##
