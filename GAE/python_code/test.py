7d5fd6db1934761944e2716a11beb6919605d370
Douglas <dug.armadale@gmail.com>
2017-06-18 11:14:40 +0100

    Tidying the test file.

--- GAE/python_code/test.py
+++ GAE/python_code/test.py
@@ -1,50 +1,32 @@
 #!/usr/bin/env python
+"""
+This was written with help from web pages
+* https://cloud.google.com/appengine/docs/python/tools/localunittesting
+* http://einaregilsson.com/unit-testing-model-classes-in-google-app-engine/
+"""
 
-# https://cloud.google.com/appengine/docs/python/tools/localunittesting
-# http://einaregilsson.com/unit-testing-model-classes-in-google-app-engine/
-
+################################################################################
 import os
 import sys
-
-sys.path.append('/home/douglas/Dropbox/publicly_visible_binaries/third_party_code_do_not_deploy/google_appengine')
-sys.path.append('/home/douglas/Dropbox/publicly_visible_binaries/third_party_code_do_not_deploy/google_appengine/lib/jinja2-2.6')
-sys.path.append('/home/douglas/Dropbox/publicly_visible_binaries/third_party_code_do_not_deploy/google_appengine/lib/yaml-3.10')
-
-
-# ? done here, the test setup, or the test ?
-path_to_templates = '../html_templates'
-
 import unittest
-import wrapper_util
+################################################################################
 
+################################################################################
+sys.path.append('/home/douglas/Dropbox/publicly_visible_binaries/third_party_code_do_not_deploy/google_appengine')
+sys.path.append('/home/douglas/Dropbox/publicly_visible_binaries/third_party_code_do_not_deploy/google_appengine/lib/yaml-3.10')
+sys.path.append('/home/douglas/Dropbox/publicly_visible_binaries/third_party_code_do_not_deploy/google_appengine/lib/jinja2-2.6')
+
+import wrapper_util
 from google.appengine.api import memcache
 from google.appengine.ext import testbed
 from google.appengine.ext import ndb
-
-# ? Are the imports done here, the test setup, or the test ?
-from tickets import sold_ticket_list
-
-
-# THE TESTS MUST BE IN THE TEST CLASS. THE CLASS SETUP MANAGES THE GAE
-# ENVIRONENT.
-
-#import configuration_database_models.py
-
-# Something wrong with the next line. Possibly need to instantiate a database first.
-# import local_lib.local_template_engine as t
-# from local_lib.local_charge_provider.charge_assume_successful_payment_if_no_error import charge_assume_successful_payment_if_no_error
-#
-# # Something wrong with the next line. Possibly need to instantiate a database first.
-#from html_generator import process_and_render_html
-#process_and_render_html.init_template_environment(path_to_templates)
-#process_and_render_html.get_index_page_html("", "")
-#
+################################################################################
 
 class DatastoreTestCase(unittest.TestCase):
+    """THE TESTS MUST BE IN THE TEST CLASS. THE CLASS SETUP MANAGES THE GAE
+    ENVIRONENT."""
 
     def setUp(self):
-
-        # ? Are the imports done here, the test setup, or the test ?
 
         # First, create an instance of the Testbed class.
         self.testbed = testbed.Testbed()
@@ -59,45 +41,38 @@ class DatastoreTestCase(unittest.TestCase):
         # using ndb.get_context().set_cache_policy(False)
         ndb.get_context().clear_cache()
 
-    # [START datastore_example_teardown]
     def tearDown(self):
+        """Datastore teardown."""
         self.testbed.deactivate()
-    # [END datastore_example_teardown]
-
+ 
     def testT(self):
 
-        # ? Are the imports done here, the test setup, or the test ?
-
         from html_generator import process_and_render_html
+        
+        path_to_templates = '../html_templates'
         process_and_render_html.init_template_environment(path_to_templates)
 
         from local_lib.local_charge_provider.charge_assume_successful_payment_if_no_error import charge_assume_successful_payment_if_no_error
 
-        #process_and_render_html.get_index_page_html("", "")
-
+        from tickets import sold_ticket_list
         sold_ticket_list.append("0", "A", "Not used")
         process_and_render_html.get_buy_tickets_dummy_charge_html({},{}, sold_ticket_list, charge_assume_successful_payment_if_no_error)
 
     def testT2(self):
 
-        # ? Are the imports done here, the test setup, or the test ?
-
         # Have a look at this an possibly change it to
         # sold_ticket_list_database
         # The dot append is confusing becaue sold_ticket_list is
         # a module.
+        from tickets import sold_ticket_list
         sold_ticket_list.append("0", "A", "Not used")
 
     def testT3(self):
-
-        # ? Are the imports done here, the test setup, or the test ?
 
         # Okay, what am I going to test html content?
         from html_generator import process_and_render_html
         print process_and_render_html.get_index_page_html("", "")
 
 if __name__ == '__main__':
-
-    # ? Are the imports done here, the test setup, or the test ?
 
     unittest.main()
